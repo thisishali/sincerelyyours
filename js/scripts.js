b2f131836cf87a3b82b788653ed0e03e4498eb6c
Hali <fanny.haller@googlemail.com>
2017-08-12 19:22:06 +0200

    NYC pictures optimized

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
     $(".gallery-start").click(function(event) {
 		var img = $(event.target);
 		var sidetext = img.siblings(".sidetext");
+		var small = img.attr("src");
+		var big = small.replace(/_preview\.jpg/, ".jpg").replace(/_preview\.JPG/, ".JPG");
+
 		$(".overlay img").replaceWith(img.clone());
+		$(".overlay img").attr("src", big)
 		$(".overlay .sidetext").replaceWith(sidetext.clone());
         $(".overlay").fadeIn();
 	});
